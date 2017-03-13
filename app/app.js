d7b341f31d8147c74391a116286c9043d5076d4a
Kazuma Ichikawa <b.g.ibx.leo10m@gmail.com>
2017-03-13 21:20:11 +0900

    複数の検索対象をマッチングできるようにする

--- app/app.js
+++ app/app.js
@@ -2,27 +2,38 @@ const cv = require("opencv")
 
 console.log("hello ioni")
 
-const BASE_IMAGE   = '/srv/base_imgs/base.jpg'
-const SEARCH_IMAGE = '/srv/search_imgs/search.jpg'
+const BASE_IMAGE_PATH   = '/srv/base_imgs/base.jpg'
+
+let searchImagePaths = [
+  '/srv/search_imgs/search1.jpg',
+  '/srv/search_imgs/search2.jpg',
+  '/srv/search_imgs/search3.jpg'
+]
 
 // 検出元の画像を読み込み
-cv.readImage(BASE_IMAGE, function(err, baseImage) {
+cv.readImage(BASE_IMAGE_PATH, function(err, baseImage) {
   if (err) return console.error('error loading image');
   baseImage.convertGrayscale()
 
-  // 検出対象画像を読み込み、マッチングを行う
-  cv.readImage(SEARCH_IMAGE, function(err, searchImage) {
-    if (err) return console.error('error loading image');
-    searchImage.convertGrayscale()
+  var results = []
+  for (var searchImagePath of searchImagePaths) {
+    // 検出対象画像を読み込み、マッチングを行う
+    cv.readImage(searchImagePath, function(err, searchImage) {
+      if (err) return console.error('error loading image');
+      searchImage.convertGrayscale()
 
-    let output = baseImage.matchTemplateByMatrix(searchImage, 3);
-    let matches = output.templateMatches(0.99, 1.0, 5, false)
-    console.log(matches)
+      let output = baseImage.matchTemplateByMatrix(searchImage, 3);
+      let matches = output.templateMatches(0.99, 1.0, 5, false)
+      console.log(matches)
 
-    if (matches.length > 0) {
-      console.log('match!')
-    } else {
-      console.log('unmatch!')
-    }
-  })
-});
+      if (matches.length > 0 && typeof(matches[0]) != 'undefined') {
+        console.log('match!')
+        results.push(true)
+      } else {
+        console.log('unmatch!')
+        results.push(false)
+      }
+    })
+  }
+  console.log(results)
+})
